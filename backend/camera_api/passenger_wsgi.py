51354ba46796da15963ffe722a884473b1376c0e
IvGyo <ivozandy@gmail.com>
2025-07-28 12:02:30 +0300

    Update passenger_wsgi.py

--- backend/camera_api/passenger_wsgi.py
+++ backend/camera_api/passenger_wsgi.py
@@ -1,1 +1,6 @@
+import os
+import sys
 
+# Променете 'fastapiCameraServer' с името на вашия основен .py файл
+# Променете 'app' с името на вашата FastAPI инстанция
+from fastapiCameraServer import app as application
